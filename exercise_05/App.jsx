711f50b2e16656994017f0471592062ad3731c37
Watsuphol <wasumc.design@gmail.com>
2024-05-28 23:53:34 +0700

    Updated

--- exercise_05/App.jsx
+++ exercise_05/App.jsx
@@ -1,14 +1,53 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const App = () => {
-	const [temperature, setTemperature] = useState(25); // Initial temperature value
-	const [isOn, setIsOn] = useState(true); // Initial visibility state
+  const [temperature, setTemperature] = useState(25); // Initial temperature value
+  const [isOn, setIsOn] = useState(true); // Initial visibility state
 
-	const decreaseTemperature = () => setTemperature((prevTemp) => prevTemp - 1);
-	const increaseTemperature = () => setTemperature((prevTemp) => prevTemp + 1);
-	const toggleIsOn = () => setIsOn(!isOn);
-	
-	return <>{/* code here */};</>;
+  const decreaseTemperature = () => setTemperature((prevTemp) => prevTemp - 1);
+  const increaseTemperature = () => setTemperature((prevTemp) => prevTemp + 1);
+  const toggleIsOn = () => setIsOn(!isOn);
+
+  return (
+    <>
+      <Header temperature={temperature} isOn={isOn} toggleIsOn={toggleIsOn} />
+      <Content temperature={temperature} />
+      <Footer
+        decreaseTemperature={decreaseTemperature}
+        increaseTemperature={increaseTemperature}
+      />
+    </>
+  );
+};
+
+const Header = ({ temperature, isOn, toggleIsOn }) => {
+  return (
+    <div>
+      <button onClick={toggleIsOn}>ON / OFF</button>
+      {isOn && <span>Current Temperature: {temperature}°C</span>}
+    </div>
+  );
+};
+
+const Content = ({ temperature }) => {
+  return (
+    <>
+      <Temperature temperature={temperature} />
+    </>
+  );
+};
+
+const Temperature = ({ temperature, isOn }) => {
+  return <div>{temperature} °C</div>;
+};
+
+const Footer = ({ decreaseTemperature, increaseTemperature }) => {
+  return (
+    <footer>
+      <button onClick={decreaseTemperature}>Down</button>
+      <button onClick={increaseTemperature}>Up</button>
+    </footer>
+  );
 };
 
 export default App;
